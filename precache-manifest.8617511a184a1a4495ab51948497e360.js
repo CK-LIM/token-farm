49d1e19cfa4a10c1b7d8a61bfe6a053800fda596
Ubuntu <cheekeat_lim1991@hotmail.com>
2021-06-24 17:20:43 +0800

    Updates

---
+++ precache-manifest.8617511a184a1a4495ab51948497e360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa1afed8e01b9922dc6d",
+    "url": "/token-farm/static/css/main.bf77cb42.chunk.css"
+  },
+  {
+    "revision": "fa1afed8e01b9922dc6d",
+    "url": "/token-farm/static/js/main.fa1afed8.chunk.js"
+  },
+  {
+    "revision": "6f855e5bb49119baafd6",
+    "url": "/token-farm/static/css/1.efb3ddd9.chunk.css"
+  },
+  {
+    "revision": "6f855e5bb49119baafd6",
+    "url": "/token-farm/static/js/1.6f855e5b.chunk.js"
+  },
+  {
+    "revision": "b281d237ee20e71d5cae",
+    "url": "/token-farm/static/js/runtime~main.b281d237.js"
+  },
+  {
+    "revision": "21a43ee626c9b7491e71700bd328d2fa",
+    "url": "/token-farm/index.html"
+  }
+];
